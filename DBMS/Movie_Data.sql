3b2704d7cd645e80a8a6d1e0afb6ca44ac2e1ace
prajaktasutarbmis <89061585+prajaktasutarbmis@users.noreply.github.com>
2021-08-30 18:19:28 +0530

    all files has added

---
+++ DBMS/Movie_Data.sql
@@ -0,0 +1,394 @@
+﻿-- Database: "Movie_Data"
+
+
+-- DROP DATABASE "Movie_Data";
+
+
+CREATE DATABASE "Movie_Data"
+
+  WITH OWNER = postgres
+
+       ENCODING = 'UTF8'
+
+       TABLESPACE = pg_default
+
+       LC_COLLATE = 'English_United States.1252'
+
+       LC_CTYPE = 'English_United States.1252'
+
+       CONNECTION LIMIT = -1;
+
+
+
+-- create the directors table
+
+CREATE TABLE directors (director_id SERIAL PRIMARY KEY,first_name VARCHAR(20),last_name VARCHAR(20) NOT NULL,date_of_birth DATE,nationality VARCHAR(10));
+
+SELECT * FROM directors;
+
+
+
+--create table actors
+ table 
+create table actors(actor_id serial primary key,first_name varchar(20),last_name varchar(20),gender char(1),date_of_birth date);
+
+select * from actors;
+
+
+
+--create movies table
+
+create table movies(movies_id serial primary key,movie_name varchar(15) not null,movie_length int,movie_language varchar(10),release_date date,age_certificate varchar(5),director_id int references directors(director_id));
+
+select * from movies;
+
+
+
+--create movie_revenues table
+
+create table movie_revenues(revenue_id serial primary key,movies_id int references movies(movies_id),domestic_takings numeric(6,2),international_takings numeric(6,2));
+
+select * from movie_revenues;
+
+
+
+--create movies_actors table
+
+create table movies_actors(movies_id int references movies(movies_id),actor_id int references actors(actor_id),primary key(movies_id,actor_id));
+
+select * from movies_actors;
+
+
+
+-- Database: mydatabase
+
+
+
+CREATE DATABASE mydatabase
+
+  WITH OWNER = postgres
+
+       ENCODING = 'UTF8'
+
+       TABLESPACE = pg_default
+
+       LC_COLLATE = 'English_United States.1252'
+
+       LC_CTYPE = 'English_United States.1252'
+
+       CONNECTION LIMIT = -1;
+
+
+
+--create employee table
+
+create table employee(employee_id serial primary key,first_name varchar(20),last_name varchar(20));
+
+select * from employee;
+
+
+
+--add new column in employee table
+
+alter table employee add column email varchar(20);
+
+
+
+--add two new columns
+
+alter table employee add column nationality varchar(20),add column age int not null;
+
+
+
+--change data type of column
+
+alter table employee alter column nationality type char(3);
+
+
+
+--insert data into table
+
+insert into employee values(101,'sooraj','shaha','sooraj@gmail.com','ind',22);
+
+insert into employee values(102,'siya','patil','siya@gmail.com','ind',21);
+
+insert into employee values(103,'emily','watson','watson@gmail.com','aus',25);
+
+insert into employee values(104,'david','warner','david@gmail.com','sa',67);
+
+insert into employee values(105,'harry','curron','harry@gmail.com','usa',54);
+
+
+
+--update age of employee whose employee id is 101
+
+update employee set age=46 where employee_id=101;
+
+
+--update employee name and email whose id is 102
+
+update employee set first_name='piya',email='piya@gmail.com' where employee_id=102;
+
+
+
+
+--delete employee whose employee id=101
+
+delete from employee where employee_id=101;
+
+select * from employee;
+
+
+
+
+--fetching data from actor table
+
+select movie_name,release_date from movies;
+
+
+
+--select first and last name of all indian directors
+
+select first_name,last_name from directors where nationality='Indian';
+
+
+
+--select all male actors born after the 1st of january1970
+
+select first_name from actors where date_of_birth > '1970-1-1';
+
+
+
+--select the names of all movies wgich are over 60 minutes long and language is Hindi
+
+select movie_name from movies where movie_length > 1 and movie_language='Hindi';
+
+
+
+--get first name and last name from actors where actors first name is shahrukh and deepika
+
+select first_name,last_name from actors where first_name in('Shahrukh','Deepika');
+
+
+
+--get first name and last name from actors where actors first name can't be shahrukh and deepika
+
+select first_name,last_name from actors where first_name not in('Shahrukh','Deepika');
+
+
+
+--get list of actors whose first name starts with 'A'
+
+select first_name from actors where first_name like 'A%';
+
+
+
+--get list of actors whose first name starts with 'A' followed by 3 characters
+
+select first_name from actors where first_name like 'A___';
+
+
+
+--get list of actors whose first name ends with 'a'
+
+select first_name from actors where first_name like '%a';
+
+
+
+--get list of actors whose first name contains 'n'
+
+select first_name from actors where first_name like '%n%';
+
+
+
+--get list of movies having release data in between 1-5-2015 and 1-5-2020
+
+select movie_name from movies where release_date between '2015-5-1' and '2020-5-1';
+
+
+
+--list of first names of actors alphabetically
+ 
+select first_name from actors order by first_name;
+
+
+
+--list of first names of actors alphabetically by descending order
+
+select first_name from actors order by first_name desc;
+
+
+
+--order by date of birth column in actors table
+
+select first_name,last_name,date_of_birth from actors order by date_of_birth;
+
+
+
+--find out lowest three domestics taking in movie_revenues
+
+select revenue_id, domestic_takings from movie_revenues order by domestic_takings limit 3;
+
+
+
+--apply offset
+
+select revenue_id, domestic_takings from movie_revenues order by revenue_id limit 3 offset 2;
+
+
+ 
+--select movie_name of movies table and fetch 1st row
+
+select movies_id,movie_name from movies fetch first 1 row only;
+
+
+
+--fetch only 3 rows
+
+select movies_id,movie_name from movies fetch first 3 row only;
+
+ 
+
+--apply offset
+
+ select movies_id,movie_name from movies offset 3 row fetch first 3 row only;
+
+
+ //get distincts movie languages in movies table
+
+ select distinct movie_language from movies;
+
+
+
+--select the actors whose date of birth is null 
+
+ select first_name from actors where date_of_birth is null;
+
+
+
+ --select the actors whose date of birth is not null
+ 
+ select first_name from actors where date_of_birth is not null;
+
+
+--rename the last_name column to surname in actors
+
+ select last_name as surname from actors;
+
+
+
+--create alias of column and list the actors whose last name starts with k order wise
+
+select last_name as surname from actors where last_name like 'K%' order by surname;
+
+ 
+
+--concat two string
+
+select 'good' || ' morning' as newstring;
+
+
+
+--concat two columns
+
+select concat(first_name,' ',last_name) as name from actors;
+
+
+
+
+  
+
+
+
+--count total number of actors
+select count(actor_id) from actors;
+--count() can not count the null values
+select count(date_of_birth) from actors;
+
+--sum the domestics takings of movie_revenues table
+select sum(domestic_takings) from movie_revenues;
+
+--get maximum value from domestics takings
+select max(domestic_takings) from movie_revenues;
+
+--get manimum value from domestics takings
+select min(domestic_takings) from movie_revenues;
+
+--get average value of domestic takings
+select avg(domestic_takings) from movie_revenues;
+--craate group of movies by movie language
+select movie_language from movies group by movie_language;
+
+--craate group of movies and count them by movie language
+select movie_language,count(movie_language) from movies group by movie_language;
+
+--craate group of movies by movie language and age certificate
+select movie_language,age_certificate from movies group by movie_language,age_certificate;
+
+--craate group of movies by movie language where movie length greater 2
+select movie_language from movies where movie_length> 2 group by movie_language;
+
+--apply having clause
+select movie_language,count(movie_length) from movies group by movie_language having count(movie_language)>1;
+
+--addition opertaion
+select 10+2 as addition;
+
+--subtraction operation
+select 10-2 as subtraction;
+
+--multiplication operation
+select 10*2 as multiplication;
+
+--division operation
+select 10/2 as division;
+
+--mod operation
+select 10%2 as mod;
+
+--inner join between two tables(table1=directors,table2=movies)
+select directors.director_id,directors.first_name,directors.last_name,movies.movie_name from directors inner join movies on directors.director_id=movies.director_id;
+
+--inner join by creating alias of tables.
+select d.director_id,d.first_name,d.last_name,m.movie_name from directors d join movies m on d.director_id=m.director_id;
+
+--inner join with 'using' keyword 
+select d.director_id,d.first_name,d.last_name,m.movie_name from directors d join movies m using (director_id);
+
+--left join between two table
+select directors.director_id,directors.first_name,directors.last_name,movies.movie_name from directors left join movies on directors.director_id=movies.director_id;
+
+--left join by creating alias of tables.
+select d.director_id,d.first_name,d.last_name,m.movie_name from directors d left join movies m on d.director_id=m.director_id;
+
+--left join with 'using' keyword 
+select d.director_id,d.first_name,d.last_name,m.movie_name from directors d left join movies m using (director_id);
+
+--right join between two table
+select directors.director_id,directors.first_name,directors.last_name,movies.movie_name from directors right join movies on directors.director_id=movies.director_id;
+
+--right join by creating alias of tables.
+select d.director_id,d.first_name,d.last_name,m.movie_name from directors d right join movies m on d.director_id=m.director_id;
+
+--right join with 'using' keyword 
+select d.director_id,d.first_name,d.last_name,m.movie_name from directors d right join movies m using (director_id);
+
+--full join between two table
+select directors.director_id,directors.first_name,directors.last_name,movies.movie_name from directors full join movies on directors.director_id=movies.director_id;
+
+--full join by creating alias of tables.
+select d.director_id,d.first_name,d.last_name,m.movie_name from directors d full join movies m on d.director_id=m.director_id;
+
+--full join with 'using' keyword 
+select d.director_id,d.first_name,d.last_name,m.movie_name from directors d full join movies m using (director_id)
+
+--join three tables
+select d.first_name,d.last_name,m.movie_name,mr.revenue_id from directors d join movies m on d.director_id=m.director_id join movie_revenues mr on m.movies_id=mr.movies_id;
+
+--select the first name and last names of all the actors who have started in movies directed by rohit shetty
+select a.first_name,a.last_name from actors a join movies_actors ma on a.actor_id=ma.actor_id join movies m on m.movies_id=ma.movies_id join directors d on d.director_id=m.director_id where d.first_name='Rohit' and d.last_name='Shetty';
+
+--which director has the highest total domestic takings
+select d.first_name,d.last_name,sum(mr.domestic_takings) as total_takings from directors d join movies m on d.director_id=m.director_id join movie_revenues mr on mr.movies_id=m.movies_id group by d.first_name,d.last_name order by total_takings desc limit 1;
+
+
---
+++ DBMS/owners_pets.sql
@@ -0,0 +1,309 @@
+﻿-- Database: owners_pets
+
+
+-- DROP DATABASE owners_pets;
+
+
+CREATE DATABASE owners_pets
+
+  WITH OWNER = postgres
+
+       ENCODING = 'UTF8'
+
+       TABLESPACE = pg_default
+
+       LC_COLLATE = 'English_United States.1252'
+
+       LC_CTYPE = 'English_United States.1252'
+<<<<<<< HEAD
+
+       CONNECTION LIMIT = -1;
+
+
+
+ --create owner table
+
+ create table owners(id serial primary key,first_name varchar(20),last_name varchar(20),city varchar(20),state character(2));
+
+select * from owners;
+
+
+
+--create pets table
+
+create table pets(pets_id serial primary key,species varchar(20),full_name varchar(10),age int,id int references owners(id));
+
+select * from pets;
+
+
+
+--add an email column in owners table
+
+alter table owners add column email varchar(10) unique;
+
+
+
+--change the data type of the last_name column in the owners table to varchar(50)
+
+alter table owners alter column last_name type varchar(50);
+
+
+
+--insert the data into owners table
+
+insert into owners values(1,'samules','smith','boston','ma','smith@gmail.com');
+
+insert into owners values(2,'emma','johnson','seattle','wa','emma@gmail.com');
+
+insert into owners values(3,'saroj','ahuja','rajkot','gj','saroj@gmail.com');
+
+insert into owners values(4,'sunny','singh','amritsar','pj','sunny@gmail.com');
+
+insert into owners values(5,'meera','deshmukh','pune','mh','meera@gmail.com');
+
+insert into owners values(6,'nill','raj','banglore','kr','nill@gmail.com');
+
+insert into owners values(7,'ajay','devara','jodhpur','rj','ajay@gmail.com');
+
+
+
+--insert the data into pets table
+
+insert into pets values(1,'dog','rex',6,1);
+
+insert into pets values(2,'rabbit','fluffy',2,5);
+
+insert into pets values(3,'cat','tom',2,5);
+
+insert into pets values(4,'mouse','jerry',2,2);
+
+insert into pets values(5,'dog','biggles',4,1);
+
+insert into pets values(6,'tortoise','squirtle',42,3);
+
+
+
+--update fluffy the rabbits age to 3
+
+update pets set age=3 where full_name='fluffy';
+
+
+
+--delete Mr raj from the owners table
+
+delete from owners where last_name='raj';
+
+
+
+
+--fetching records from movie database
+--
+select movie-name and release-date of every movie
+
+select movie_name,release_date from movies;
+
+
+
+--select first and last name of all indian directors
+
+select first_name,last_name from directors where nationality='Indian';
+
+
+
+--select all male actors born after the 1st of january1970
+
+select first_name from actors where date_of_birth > '1970-1-1';
+
+
+
+--select the names of all movies which are over 60 minutes long and language is Hindi
+
+select movie_name from movies where movie_length > 1 and movie_language='Hindi';
+
+
+
+--select the movie names and movie language of all movies with a movie language of English,Hindi
+
+select movie_name,movie_language from movies where movie_language in('English','Hindi');
+
+
+
+
+--select first name and last name of the actors whose last name begins with K and were born between 1/1/1960 and 31/12/1979
+
+select first_name,last_name from actors where last_name like 'K%' and date_of_birth between '1960-1-1' and '1979-12-31';
+
+
+
+--select the first name and last name of director with nationality of american,Indian born between 1/1/1950 and 31/12/1980
+
+select first_name,last_name from directors where nationality in ('american','Indian') and date_of_birth between '1950-1-1' and '1980-12-31';
+
+
+
+ --select the indian directors ordered from oldest to youngest
+ 
+select first_name,last_name,date_of_birth from directors where nationality='Indian' order by date_of_birth;
+
+
+
+ --return the distinct nationalities from directors table;
+ 
+select distinct nationality from directors;
+
+ 
+
+--return the first names,last names and date of births of the 2 youngest female actors.
+ 
+select first_name,last_name,date_of_birth from actors where gender='F' order by date_of_birth desc limit 2;
+
+
+
+--return the top movies with the highest international takings
+
+select movies_id from movie_revenues order by international_takings desc limit 1;
+
+
+
+--concatenate the first and last name of the directors seperated by a space,and call this full new column full_name
+
+select concat(first_name,' ',last_name) as full_name from directors;
+
+
+
+--return the actors with missing first_name or missing date_of_births
+
+select actor_id,first_name from actors where first_name is null or date_of_birth is null;
+
+
+
+
+
+
+
+
+
+
+=======
+       CONNECTION LIMIT = -1;
+ --create owner table
+ create table owners(id serial primary key,first_name varchar(20),last_name varchar(20),city varchar(20),state character(2));
+select * from owners;
+
+--create pets table
+create table pets(pets_id serial primary key,species varchar(20),full_name varchar(10),age int,id int references owners(id));
+select * from pets;
+
+--add an email column in owners table
+alter table owners add column email varchar(10) unique;
+
+--change the data type of the last_name column in the owners table to varchar(50)
+alter table owners alter column last_name type varchar(50);
+
+--insert the data into owners table
+insert into owners values(1,'samules','smith','boston','ma','smith@gmail.com');
+insert into owners values(2,'emma','johnson','seattle','wa','emma@gmail.com');
+insert into owners values(3,'saroj','ahuja','rajkot','gj','saroj@gmail.com');
+insert into owners values(4,'sunny','singh','amritsar','pj','sunny@gmail.com');
+insert into owners values(5,'meera','deshmukh','pune','mh','meera@gmail.com');
+insert into owners values(6,'nill','raj','banglore','kr','nill@gmail.com');
+insert into owners values(7,'ajay','devara','jodhpur','rj','ajay@gmail.com');
+
+--insert the data into pets table
+insert into pets values(1,'dog','rex',6,1);
+insert into pets values(2,'rabbit','fluffy',2,5);
+insert into pets values(3,'cat','tom',2,5);
+insert into pets values(4,'mouse','jerry',2,2);
+insert into pets values(5,'dog','biggles',4,1);
+insert into pets values(6,'tortoise','squirtle',42,3);
+
+--update fluffy the rabbits age to 3
+update pets set age=3 where full_name='fluffy';
+
+--delete Mr raj from the owners table
+delete from owners where last_name='raj';
+
+--select movie-name and release-date of every movie
+select movie_name,release_date from movies;
+
+--select first and last name of all indian directors
+select first_name,last_name from directors where nationality='Indian';
+
+--select all male actors born after the 1st of january1970
+select first_name from actors where date_of_birth > '1970-1-1';
+
+--select the names of all movies wgich are over 60 minutes long and language is Hindi
+select movie_name from movies where movie_length > 1 and movie_language='Hindi';
+
+--get first name and last name from actors where actors first name is shahrukh and deepika
+select first_name,last_name from actors where first_name in('Shahrukh','Deepika');
+
+--get first name and last name from actors where actors first name can't be shahrukh and deepika
+select first_name,last_name from actors where first_name not in('Shahrukh','Deepika');
+
+--select the movie names and movie language of all movies with a movie language of English,Hindi
+select movie_name,movie_language from movies where movie_language in('English','Hindi');
+
+--select first name and last name of the actors whose last name begins with K and were born between 1/1/1960 and 31/12/1979
+select first_name,last_name from actors where last_name like 'K%' and date_of_birth between '1960-1-1' and '1979-12-31';
+
+--select the first name and last name of director with nationality of american,Indian born between 1/1/1950 and 31/12/1980
+select first_name,last_name from directors where nationality in ('american','Indian') and date_of_birth between '1950-1-1' and '1980-12-31';
+
+ --select the indian directors ordered from oldest to youngest
+ select first_name,last_name,date_of_birth from directors where nationality='Indian' order by date_of_birth;
+
+ --return the distinct nationalities from directors table;
+ select distinct nationality from directors;
+
+ --return the first names,last names and date of births of the 2 youngest female actors.
+ select first_name,last_name,date_of_birth from actors where gender='F' order by date_of_birth desc limit 2;
+
+--return the top movies with the highest international takings
+select movies_id from movie_revenues order by international_takings desc limit 1;
+
+--concatenate the first and last name of the directors seperated by a space,and call this full new column full_name
+select concat(first_name,' ',last_name) as full_name from directors;
+
+--return the actors with missing first_name or missing date_of_births
+select actor_id,first_name from actors where first_name is null or date_of_birth is null;
+
+--count the number of actors born after the 1st of january 1980
+select count(actor_id) from actors where date_of_birth >'1980-1-1';
+
+--what was the heighest and lowest domestic takings for movie;
+select max(domestic_takings) from movie_revenues;
+select min(domestic_takings) from movie_revenues;
+
+--what is the sum total movie length for movies rated 15;
+select sum(movie_length) from movies where age_certificate='18';
+
+--how many Indian directors in the directors table
+select count(director_id) from directors where nationality='Indian';
+
+--what is the average movie length for hindi movies; 
+select avg(movie_length) from movies where movie_language='Hindi';
+
+--how many directors are there per nationality
+select nationality,count(nationality) from directors group by nationality;
+
+--what is the sum total movie length for each age certificate and movie language combination
+select movie_language,age_certificate,sum(movie_length) from movies group by movie_language,age_certificate;
+
+--return the movie language which have a sum total movie length of over 60 mins
+select movie_language,sum(movie_length) from movies group by movie_language having sum(movie_length)>1;
+
+--select the directors first name and last name,the movie names and release dates for all Hindi,Marathi and English movies
+select d.first_name,d.last_name,m.movie_name,m.release_date from directors d join movies m on d.director_id=m.director_id where m.movie_language in('Hindi','English','Marathi');
+
+--select  the movie names,release dates and international taking of all Hindi language movies
+select m.movie_name,m.release_date,mr.international_takings from movies m join movie_revenues mr on m.movies_id=mr.movies_id where m.movie_language='Hindi';
+
+--select the movies names,domestic takings and international takings for all movies with either missing domestic takings or missing international takings and order the results by movie name
+select m.movie_name,mr.domestic_takings,mr.international_takings from movies m join movie_revenues mr on m.movies_id=mr.movies_id where mr.domestic_takings is null or mr.international_takings is null order by m.movie_name;
+
+--use a left join to select the first and last names of all Indian directors and names and age certificates of the movies that they directed
+select d.first_name,d.last_name,m.movie_name,m.age_certificate from directors d left join movies m on d.director_id=m.director_id where d.nationality='Indian';
+
+--count the number of movies that each director has directed
+select d.first_name,d.last_name,count(m.movies_id) from directors d left join movies m on d.director_id=m.director_id group by d.first_name,d.last_name; 
