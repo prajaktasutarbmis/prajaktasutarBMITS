fcd838f4fd00461d7211c234ad27cbe1f14e12d3
prajaktasutarbmis <89061585+prajaktasutarbmis@users.noreply.github.com>
2021-08-30 18:22:09 +0530

    All files has uploaded

--- JS/ExportDemo.js
+++ JS/ExportDemo.js
@@ -1,7 +1,26 @@
-//Default export class
+//Named export class
 class Demo{
     show(){
-        console.log('Nice to see to you');
+        console.log('You are in Demo class');
     }
 }
-export default Demo;
+export {Demo};
+
+//Named export function
+export function fun()
+{
+    console.log('You are in fun() function');
+}
+
+//Named Export variable
+let age=22;
+export {age};
+
+//Default Export class
+export default class Calculate{
+    add()
+    {
+        return 2+3;
+    }
+}
+
--- JS/ImportDemo.js
+++ JS/ImportDemo.js
@@ -1,4 +1,12 @@
-//Default import
-import Demo from "./ExportDemo.js";    
+//Named import
+import {Demo,fun,age} from "./ExportDemo.js";
 var ob=new Demo();
 ob.show();
+fun();
+console.log('Your age is '+age);
+
+//Default import
+import Calculate from "./ExportDemo.js";
+var ob2=new Calculate();
+var answer=ob2.add();
+console.log('Addition='+answer);
