985e9ac4dac280240b5cc2accdb28de14ddf6c96
prajaktasutarbmis <89061585+prajaktasutarbmis@users.noreply.github.com>
2021-09-01 23:26:28 +0530

    Basic project files has added

---
+++ ReactTutorials/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from './Card';
+function ExpenseItem(props){
+    //const expenseDate=new Date(2021,2,28);
+    //const expenseTitle='car insurance';
+    //const expenseAmount=254.78;
+    
+    return(
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </Card>
+    );
+}
+
+export default ExpenseItem;
---
+++ ReactTutorials/components/Expenses.js
@@ -0,0 +1,14 @@
+ import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+import Card from "./Card";
+ function Expenses(props)
+{
+    return (
+        <Card className="expenses">
+            <h1>Hello,let's gets started</h1>
+            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}></ExpenseItem>
+            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}></ExpenseItem>
+            <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}></ExpenseItem>
+    </Card>);
+}
+export default Expenses;
