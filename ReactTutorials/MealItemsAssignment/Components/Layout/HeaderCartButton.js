ffb5eb1df478f69d2048320f0f06918a350dc38b
prajaktasutarbmis <89061585+prajaktasutarbmis@users.noreply.github.com>
2021-09-07 21:04:34 +0530

    Added all layout related files

---
+++ ReactTutorials/MealItemsAssignment/Components/Layout/HeaderCartButton.js
@@ -0,0 +1,18 @@
+import classes from './HeaderCartButton.module.css';
+import CartContext from '../../Store/cart-context';
+import CartIcon from '../Cart/CartIcon';
+import { useContext } from 'react';
+const HeaderCartButton=props=>{
+    const cartCtx=useContext(CartContext);
+    const numberOfCartItems=cartCtx.items.reduce((currentNumber,item)=>{return currentNumber+item.amount},0);
+    return (
+    <button className={classes.button} onClick={props.onClick}>
+        <span className={classes.icon}>
+            <CartIcon />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>);
+
+}
+export default HeaderCartButton;
